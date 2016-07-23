21b53b1792c0f986ef9c6847498a7bb435d1ba82
Adam Ross Russell <adamrr724@yahoo.com>
2016-07-23 13:52:33 -0700

    add eventype to the model data

--- server/boot/create-table.js
+++ server/boot/create-table.js
@@ -1,13 +1,13 @@
 module.exports = function(app) {
-  app.dataSources.sportaldb.automigrate('Event', function(err) {
-    if (err) throw err;
-
-    app.models.Event.create([
-      {name: 'Test', sport: 'test', notes: 'test', skillLevel: 'test', recurring: '1', age: 'test', fees: 'test' },
-  ], function(err, Events) {
-      if (err) throw err;
-
-      console.log('Models created: \n', Events);
-    });
-  });
+  // app.dataSources.sportaldb.automigrate('Event', function(err) {
+  //   if (err) throw err;
+  //
+  //   app.models.Event.create([
+  //     {eventType: 'Test', name: 'Test', sport: 'test', notes: 'test', skillLevel: 'test', recurring: '1', age: 'test', fees: 'test' },
+  // ], function(err, Events) {
+  //     if (err) throw err;
+  //
+  //     console.log('Models created: \n', Events);
+  //   });
+  // });
 };
